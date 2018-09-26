1adca195c0b75ff03326bef1489991ebabfd2a27
davidfitz314 <davidfitz314@gmail.com>
2018-09-26 15:17:22 -0600

    testing image

--- keyarraP.php
+++ keyarraP.php
@@ -3,14 +3,15 @@
   <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <header>
-  <h1>Welcome Keyarra</h1>
+  <h1>Welcome to Keyarra's Life</h1>
 </header>
 <body>
-  <p>Since you traded your photos legally for cash, I wanted to show you a preview of what your website can look like.</p>
-  <p>But in the event you wish to regain your photos rights, you can finish the deal we made.<p>
-    <p>Now in the event you don't want to finish the deal, well this website will be updated into the key blog with its own unique keyarrablog.com url, and each day i will upload one of my photos, with extra interesting information.</p>
+  <p>Born September 28th, 1999. I wanted a way to express myself so I sold my photos to this blog.</p>
+  <p>This website will be updated with its own unique keyarrablog.com url on september 27th, 2018.</p>
   <img src="LegalOwnership/Snapchat-210759784.jpg" alt="key1">
+  <p>Ugh Today sucks, no one complimented my new awesome shirt.</p>
   <img src="LegalOwnership/Snapchat-227986906.jpg" alt="key2">
+  <p>Super hot today in st geezy, but at least my tan lines rock!</p>
   <img src="LegalOwnership/Snapchat-326844518.jpg" alt="key3">
   <img src="LegalOwnership/Snapchat-805213756.jpg" alt="key4">
   <img src="LegalOwnership/Snapchat-834282903.jpg" alt="key5">
