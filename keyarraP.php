f8fee6e68914be7cb7bcd72b307d77857c96e844
davidfitz314 <davidfitz314@gmail.com>
2018-09-26 14:29:57 -0600

    example site

---
+++ keyarraP.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<header>
+  <h1>Welcome Keyarra</h1>
+</header>
+<body>
+  <p>Since you traded your photos legally for cash, I wanted to show you a preview of what your website can look like.</p>
+  <p>But in the event you wish to regain your photos rights, you can finish the deal we made.<p>
+</body>
+</html>
---
+++ keyarraPhotos.php
@@ -0,0 +1,8 @@
+<html>
+<header>
+<h1>Welcome Keyarra</h1>
+</header>
+<body>
+  <p>This is a preview of what your website can look like. unless of course you finish your side of the promise. Also quick note thank you for selling your photos to me, if you don't finish the deal, i can legally do what ever I feel like with them.</p>
+</body>
+</html>
