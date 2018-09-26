f356dd84202629211ca5f51c89d7293780a019a2
davidfitz314 <davidfitz314@gmail.com>
2018-09-26 15:05:48 -0600

    testing image

--- keyarraP.php
+++ keyarraP.php
@@ -8,5 +8,15 @@
 <body>
   <p>Since you traded your photos legally for cash, I wanted to show you a preview of what your website can look like.</p>
   <p>But in the event you wish to regain your photos rights, you can finish the deal we made.<p>
+    <p>Now in the event you don't want to finish the deal, well this website will be updated into the key blog. with a daily update, until you can keep your word.</p>
+  <img src="LegalOwnership/Snapchat-210759784.jpg" alt="key1">
+  <img src="LegalOwnership/Snapchat-227986906.jpg" alt="key2">
+  <img src="LegalOwnership/Snapchat-326844518.jpg" alt="key3">
+  <img src="LegalOwnership/Snapchat-805213756.jpg" alt="key4">
+  <img src="LegalOwnership/Snapchat-834282903.jpg" alt="key5">
+  <img src="LegalOwnership/Snapchat-851951769.jpg" alt="key6">
+  <img src="LegalOwnership/Snapchat-1668984211.jpg" alt="key7">
+  <img src="LegalOwnership/Snapchat-1852124383.jpg" alt="key8">
+  <img src="LegalOwnership/Snapchat-2120334385" alt="key9">
 </body>
 </html>
