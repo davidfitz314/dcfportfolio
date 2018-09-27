a4feb1d377c6c8c30d7a6c79b565251c499238cf
davidfitz314 <davidfitz314@gmail.com>
2018-09-26 21:00:38 -0600

    updated key page

--- keyarraP.php
+++ keyarraP.php
@@ -6,8 +6,6 @@
   <h1> Thank you Keyarra, I appreciate you and I'm sorry I got mad.</h1>
 </header>
 <body>
-  <p>be happy!!!</p>
-  <img src="icons/be-happy.jpg" alt="be happy" width="50%" height="auto">
 </body>
 <footer>
 </footer>
