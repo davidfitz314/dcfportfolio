009694f370f803035cad194c715c7a1b43adbac7
davidfitz314 <davidfitz314@gmail.com>
2018-09-27 19:43:02 -0600

    icon images insert

--- footer.php
+++ footer.php
@@ -2,11 +2,12 @@
 
     <div class="connectIcons">
       <img src="images/facebook.png" alt="Facebook link">
-      <img src="images/github-logo.png" alt="Github link">
       <img src="images/linkedin.png" alt="Linkedin link">
+      <img src="images/github-logo.png" alt="Github link">
     </div>
-    <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-
+    <div class="noLinkEffects">
+      <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    </div>
 </div>
 </footer>
 </body>
