574ae065818ee759c1b152ad84b3bfe4a570ec99
davidfitz314 <davidfitz314@gmail.com>
2018-09-27 19:31:54 -0600

    icon images insert

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
 <footer>
 
-    <p>Sponser information</p>
+    <div class="connectIcons">
+      <img src="images/facebook.png" alt="Facebook link">
+      <img src="images/github-logo.png" alt="Github link">
+      <img src="images/linkedin.png" alt="Linkedin link">
+    </div>
     <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-  </div>
+</div>
 </footer>
 </body>
 </html>
