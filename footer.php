debc3463eb05ffd528f830a941c83233f95c3ed5
davidfitz314 <davidfitz314@gmail.com>
2018-09-27 23:37:33 -0600

    updating fonts for footer

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
       <img src="images/github-logo.png" alt="Github link">
     </div>
     <div class="noLinkEffects">
-      <div><p>Facebook, Github, and Linkedin Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC BY 3.0</a></p></div>
+      <div>Facebook, Github, and Linkedin Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC BY 3.0</a></div>
     </div>
 </div>
 </footer>
