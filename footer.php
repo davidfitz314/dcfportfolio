7acfdd2577e337aeb8d69e646312974ccb54850e
davidfitz314 <davidfitz314@gmail.com>
2018-09-27 19:39:14 -0600

    icon images insert

--- footer.php
+++ footer.php
@@ -5,7 +5,8 @@
       <img src="images/github-logo.png" alt="Github link">
       <img src="images/linkedin.png" alt="Linkedin link">
     </div>
-    <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+
 </div>
 </footer>
 </body>
