13bd81de2932e3b1093f5bd5016583a4c9e59523
davidfitz314 <davidfitz314@gmail.com>
2018-09-27 18:26:56 -0600

    realigning nav menu items

--- header.php
+++ header.php
@@ -23,12 +23,6 @@
     </div>
   </div>
 
-  <!-- links to about section -->
-  <div class="topBanner">
     <header>
-      <h1>DCFPortfolio</h1>
-      <p>welcome to my portfolio, my name is David ChenFitzgerald. blah blah blah.</p>
-      <p>Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, </p>
-      <p>Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum,</p>
     </header>
   </div>
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php include 'header.php'; ?>
 <div class="centeredWrapper">
-
+  <!-- links to about section -->
+  <div class="topBanner">
+    <h1>DCFPortfolio</h1>
+    <p>welcome to my portfolio, my name is David ChenFitzgerald. blah blah blah.</p>
+    <p>Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, </p>
+    <p>Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum,</p>
+  </div>
   <p> test 8 .7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777</p>
 
 </div>
