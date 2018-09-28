1fac081f919f5ace31bee550bb0440ab66c8b865
davidfitz314 <davidfitz314@gmail.com>
2018-09-27 22:11:19 -0600

    testing banner quick link and hover

--- index.php
+++ index.php
@@ -2,21 +2,25 @@
 <div class="centeredWrapper">
   <!-- links to about section -->
   <div class="aboutBanner">
-    <div class="AboutMeWrapperGoLeft">
-      <h2>Welcome to my Portfolio,</h2>
-      <h3>My name is David ChenFitzgerald and I am a Software Developer and IT technician.</h3>
-      <p>I love Computer Programming and Information Technology.<br />
-        I have a Bachelors in CIT emphasizing CS.<br />
-        I am an accomplishment oriented person, it’s similar to goal oriented,
-        but instead, I try to see the big picture alongside of each smaller goal.<br />
-        I am great at working in teams, and I enjoy communicating and getting to know about my project partners.<br />
-        I have been in customer service for quite some time and I have learned to excel at it,
-        by giving customers great support. <br />
-        I am also an easy going person who knows how to get tasks done on time.</p>
-      <p>continue reading...</p>
-    </div>
-    <div class="AboutMeWrapperGoRight">
-      <img src="images/filler-image.jpg" alt="Filler Image" width="200px" height="250px">
+    <div class="bannerLinks">
+    <a href="index">
+      <div class="AboutMeWrapperGoLeft">
+        <h2>Welcome to my Portfolio,</h2>
+        <h3>My name is David ChenFitzgerald and I am a Software Developer and IT technician.</h3>
+        <p>I love Computer Programming and Information Technology.<br />
+          I have a Bachelors in CIT emphasizing CS.<br />
+          I am an accomplishment oriented person, it’s similar to goal oriented,
+          but instead, I try to see the big picture alongside of each smaller goal.<br />
+          I am great at working in teams, and I enjoy communicating and getting to know about my project partners.<br />
+          I have been in customer service for quite some time and I have learned to excel at it,
+          by giving customers great support. <br />
+          I am also an easy going person who knows how to get tasks done on time.</p>
+        <p>continue reading...</p>
+      </div>
+      <div class="AboutMeWrapperGoRight">
+        <img src="images/filler-image.jpg" alt="Filler Image" width="200px" height="250px">
+      </div>
+    </a>
     </div>
   </div>
 
