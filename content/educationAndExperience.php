8f3daaac024f871f80506279dfa40486d1b27d52
davidfitz314 <davidfitz314@gmail.com>
2018-10-02 15:35:53 -0600

    populating pages with base/trial data

--- content/educationAndExperience.php
+++ content/educationAndExperience.php
@@ -1,7 +1,7 @@
 <?php include 'defaultIncludes/header.php'; ?>
 <div class="centeredWrapper">
 <h3>Education</h3>
-<img src="images/" alt="Dixie State University">
+<img src="images/dsu-logo.jpg" alt="Dixie State University">
 <h4>Dixie State University</h4>
 <p>place address</p>
 <p>Graduated in 2017 with a Bachelors in Computer information Technology Emphasizing Computer Science</p>
