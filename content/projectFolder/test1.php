8a7cc9e7e80de0f079e4e30b2ae576b3185ade24
davidfitz314 <davidfitz314@gmail.com>
2018-10-02 16:22:07 -0600

    confirming new project firectories and links work correctly

--- content/projectFolder/test1.php
+++ content/projectFolder/test1.php
@@ -1,5 +1,7 @@
 <?php include '../defaultIncludes/header.php'; ?>
 <div class="centeredWrapper">
 <a href="../projects">Return to projects directory</a>
+<hr />
+<p>test1 complete</p>
 </div>
 <?php include '../defaultIncludes/footer.php'; ?>
