42a334d0d211070d0d5c6c457a9b833aedbf1191
davidfitz314 <davidfitz314@gmail.com>
2018-10-02 04:12:14 -0600

    checking about me footer placement

--- content/aboutMe.php
+++ content/aboutMe.php
@@ -1,5 +1,6 @@
 <?php include 'defaultIncludes/header.php'; ?>
 <!-- centerred wrapper div ends in the footer -->
-<div class="centeredWrapper"> 
+<div class="centeredWrapper">
   <p>tesssssssssssssttttttttt</p>
+</div>
 <?php include 'defaultIncludes/footer.php'; ?>
