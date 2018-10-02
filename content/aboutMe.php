ce0cd33dd098f0df8a5b86baa6c70d45a13f5442
davidfitz314 <davidfitz314@gmail.com>
2018-10-02 04:08:53 -0600

    update the index links

--- content/aboutMe.php
+++ content/aboutMe.php
@@ -1,5 +1,5 @@
 <?php include 'defaultIncludes/header.php'; ?>
 <!-- centerred wrapper div ends in the footer -->
-<div class="centeredWrapper">
+<div class="centeredWrapper"> 
   <p>tesssssssssssssttttttttt</p>
 <?php include 'defaultIncludes/footer.php'; ?>
