5655c81a303d989ee59667934f0d38cc41c69da3
davidfitz314 <davidfitz314@gmail.com>
2018-10-02 15:59:04 -0600

    adding temp buttons to project.php folder overview

--- content/projects.php
+++ content/projects.php
@@ -2,5 +2,19 @@
 <div class="centeredWrapper">
 <p>check out test1 project</p>
 <a href="projectFolder/test1.php">test1</a>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
+<button>filler</button>
 </div>
 <?php include 'defaultIncludes/footer.php'; ?>
