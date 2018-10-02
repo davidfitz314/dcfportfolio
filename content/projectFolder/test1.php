b852b080ed03c8de812a3ef7d5c0fcfce63eb499
davidfitz314 <davidfitz314@gmail.com>
2018-10-02 15:15:01 -0600

    creating projects container and testing returns using links

---
+++ content/projectFolder/test1.php
@@ -0,0 +1,5 @@
+<?php include '../defaultIncludes/header.php'; ?>
+<div class="centeredWrapper">
+<a href="../">Return to projects directory</a>
+</div>
+<?php include '../defaultIncludes/footer.php'; ?>
--- content/projects.php
+++ content/projects.php
@@ -1,5 +1,6 @@
 <?php include 'defaultIncludes/header.php'; ?>
 <div class="centeredWrapper">
-
+<p>check out test1 project</p>
+<a href="projectFolder/test1.php">test1</a>
 </div>
 <?php include 'defaultIncludes/footer.php'; ?>
