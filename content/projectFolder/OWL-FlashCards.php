4c590a8f133385f5aff0cb0ec413226715e0005a
davidfitz314 <davidfitz314@gmail.com>
2018-10-05 13:07:47 -0600

    adding in project owl-flashcards page

---
+++ content/projectFolder/OWL-FlashCards.php
@@ -0,0 +1,10 @@
+<?php include '../defaultIncludes/header.php'; ?>
+<div class="centeredWrapper">
+<a href="../projects">Return to projects directory</a>
+<hr />
+<p>OWL-FlashCards</p>
+<p>Our World Languages Flash Cards</p>
+<p>by DCFApps</p>
+<p><i>insert pagination at base of each project page</i></p>
+</div>
+<?php include '../defaultIncludes/footer.php'; ?>
