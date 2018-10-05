6b5e176d56fbfdd8264a025a19e377882d514e2b
davidfitz314 <davidfitz314@gmail.com>
2018-10-05 14:12:49 -0600

    testing a dropdown menu instead of nav bar

--- content/defaultIncludes/header.php
+++ content/defaultIncludes/header.php
@@ -18,7 +18,15 @@
 
 
         <div class="goRight">
-          <img src="/content/images/menuButton.png" alt="menu-button" width="43px" height="auto">
+          //creating a dropdown menu
+          <div class="dropdown">
+            <button class="dropbtn"><img src="/content/images/menuButton.png" alt="menu-button" width="43px" height="auto"></button>
+            <div class="dropdown-content">
+              <a href="/content/aboutMe">About</a>
+              <a href="/content/projects">Projects</a>
+              <a href="/content/educationAndExperience">Education</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
