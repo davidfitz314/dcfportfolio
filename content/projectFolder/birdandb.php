59c460cdcd2553f980aaa9c800606f0c36bc9f4e
davidfitz314 <davidfitz314@gmail.com>
2018-10-05 14:45:35 -0600

    correcting linking for birdandb.com

--- content/projectFolder/birdandb.php
+++ content/projectFolder/birdandb.php
@@ -3,7 +3,7 @@
 <a href="../projects">Return to projects directory</a>
 <hr />
 <p>BirdAndB.com is a website I made to help aid young adults in learning about psyiology and biology of life.</p>
-<a href="http://http://www.birdandb.com/" target="_blank"><b>Click Here to Visit birdandb.com</b></a>
+<a href="http://www.birdandb.com/" target="_blank"><b>Click Here to Visit birdandb.com</b></a>
 <p><i>insert pagination at base of each project page</i></p>
 </div>
 <?php include '../defaultIncludes/footer.php'; ?>
