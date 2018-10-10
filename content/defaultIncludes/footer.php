6b9e877ac3eb9cfaaea44e62c053b9c38a0b7a80
davidfitz314 <davidfitz314@gmail.com>
2018-10-09 23:07:06 -0600

    styling the footer into sections

--- content/defaultIncludes/footer.php
+++ content/defaultIncludes/footer.php
@@ -8,9 +8,9 @@
     <h4>connect</h4>
     <p>What to say about this website to improve search optimization? I don't know yet but I can add it or a second logo.</p>
     <div class="connectIcons">
-      <a href="https://www.facebook.com/david.fitzgerald.528" target="_blank"><img src="/content/images/facebook.png" alt="Facebook link"></a>
-      <a href="https://www.linkedin.com/in/david-chenfitzgerald/" target="_blank"><img src="/content/images/linkedin.png" alt="Linkedin link"></a>
-      <a href="https://github.com/davidfitz314" target="_blank"><img src="/content/images/github-logo.png" alt="Github link"></a>
+      <a href="https://www.facebook.com/david.fitzgerald.528" target="_blank"><img src="/content/images/facebook_edit.png" alt="Facebook link"></a>
+      <a href="https://www.linkedin.com/in/david-chenfitzgerald/" target="_blank"><img src="/content/images/linkedin_edit.png" alt="Linkedin link"></a>
+      <a href="https://github.com/davidfitz314" target="_blank"><img src="/content/images/github-logo.png_edit" alt="Github link"></a>
     </div>
     <div class="noLinkEffects">
       <div>Facebook, Github, and Linkedin Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC BY 3.0</a></div>
