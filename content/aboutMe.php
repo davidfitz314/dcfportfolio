484846032bd37a601317b3b37c2589b70a649ab8
davidfitz314 <davidfitz314@gmail.com>
2018-10-10 01:27:41 -0600

    grammer correction

--- content/aboutMe.php
+++ content/aboutMe.php
@@ -3,7 +3,7 @@
   <img src="images/davidNormal1.jpg" alt="Everyday Dave" width="300px" height="300px">
   <p>My name is David ChenFitzgerald. I am a Computer Programmer and It Technician.
   I live in St. George UT, and I love travelling to new places. I got married in 2017 to a beautiful Chinese graphic web designer and from then on have improved my own apps greatly due to her influence.
-  I currently take on Freelance programming projects the most, because I feel every person has new and interesting app ideas but they just don't know how to publish them, so i do my best to help.
+  I currently take on freelance programming projects the most, because I feel every person has new and interesting app ideas but they just don't know how to publish them, so i do my best to help.
   </p>
   <p>Extra Interests</p>
   <p>I volunteer to help teach kids programming, because I believe the worlds technology will only grow greater with new minds coming to help.
