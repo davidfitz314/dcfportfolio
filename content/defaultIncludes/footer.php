afb1133ec4ecd8d718e975457500810c375647e2
davidfitz314 <davidfitz314@gmail.com>
2018-10-09 23:03:35 -0600

    styling the footer into sections

--- content/defaultIncludes/footer.php
+++ content/defaultIncludes/footer.php
@@ -1,40 +1,44 @@
 <footer>
   <!-- end centerred wrapper div?-->
   </div>
-<!-- keywords for search optimization example: https://www.orbitmedia.com/blog/website-footer-design-best-practices/ -->
-<div class="leftFooter">
-  <h4>connect</h4>
-  <p>What to say about this website to improve search optimization? I don't know yet but I can add it or a second logo.</p>
-  <div class="connectIcons">
-    <a href="https://www.facebook.com/david.fitzgerald.528" target="_blank"><img src="/content/images/facebook.png" alt="Facebook link"></a>
-    <a href="https://www.linkedin.com/in/david-chenfitzgerald/" target="_blank"><img src="/content/images/linkedin.png" alt="Linkedin link"></a>
-    <a href="https://github.com/davidfitz314" target="_blank"><img src="/content/images/github-logo.png" alt="Github link"></a>
+
+<div class="threeSections">
+  <!-- keywords for search optimization example: https://www.orbitmedia.com/blog/website-footer-design-best-practices/ -->
+  <div class="leftFooter">
+    <h4>connect</h4>
+    <p>What to say about this website to improve search optimization? I don't know yet but I can add it or a second logo.</p>
+    <div class="connectIcons">
+      <a href="https://www.facebook.com/david.fitzgerald.528" target="_blank"><img src="/content/images/facebook.png" alt="Facebook link"></a>
+      <a href="https://www.linkedin.com/in/david-chenfitzgerald/" target="_blank"><img src="/content/images/linkedin.png" alt="Linkedin link"></a>
+      <a href="https://github.com/davidfitz314" target="_blank"><img src="/content/images/github-logo.png" alt="Github link"></a>
+    </div>
+    <div class="noLinkEffects">
+      <div>Facebook, Github, and Linkedin Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC BY 3.0</a></div>
+    </div>
   </div>
-  <div class="noLinkEffects">
-    <div>Facebook, Github, and Linkedin Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC BY 3.0</a></div>
+
+  <!-- create a small recent projects section here, with textual description and links -->
+  <div class="middleFooter">
+    <h4>recent projects</h4>
+    <ul>
+      <li><a href="/content/projectFolder/index.php">The current website that you are on. ;)</a></li>
+      <li><a href="/content/projectFolder/OWL-Flashcards.php">Practice learning Chinese/English by using flashcards of English most common words.</a></li>
+      <li><a href="/content/projectFolder/birdandb.php">A website designed to help aid learning for biological and psyiological functions in a reporoductive context.</a></li>
+    </ul>
   </div>
-</div>
 
-<!-- create a small recent projects section here, with textual description and links -->
-<div class="middleFooter">
-  <h4>recent projects</h4>
-  <ul>
-    <li><a href="/content/projectFolder/index.php">The current website that you are on. ;)</a></li>
-    <li><a href="/content/projectFolder/OWL-Flashcards.php">Practice learning Chinese/English by using flashcards of English most common words.</a></li>
-    <li><a href="/content/projectFolder/birdandb.php">A website designed to help aid learning for biological and psyiological functions in a reporoductive context.</a></li>
-  </ul>
-</div>
+  <!-- create a second navigation menu -->
 
-<!-- create a second navigation menu -->
-
-<div class="rightFooter">
-  <h4>navigate</h4>
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="aboutMe.php">About Me</a></li>
-    <li><a href="projects.php">Projects</a></li>
-    <li><a href="education.php">Education</a></li>
-  </ul>
+  <div class="rightFooter">
+    <h4>navigate</h4>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="aboutMe.php">About Me</a></li>
+      <li><a href="projects.php">Projects</a></li>
+      <li><a href="education.php">Education</a></li>
+    </ul>
+  </div>
+<!-- end threesections-->
 </div>
 
 <!-- add in a sitemap after website completion -->
