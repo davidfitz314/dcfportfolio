0b1806af111091b4467fe411b2d3d71d442bb47c
davidfitz314 <davidfitz314@gmail.com>
2018-10-09 22:01:57 -0600

    adding in links to the footer for site map and copyright

--- content/defaultIncludes/footer.php
+++ content/defaultIncludes/footer.php
@@ -1,14 +1,20 @@
 <footer>
 
     <div class="connectIcons">
-      <img src="/content/images/facebook.png" alt="Facebook link">
-      <img src="/content/images/linkedin.png" alt="Linkedin link">
-      <img src="/content/images/github-logo.png" alt="Github link">
+      <a href="https://www.facebook.com/david.fitzgerald.528" target="_blank"><img src="/content/images/facebook.png" alt="Facebook link"></a>
+      <a href="https://www.linkedin.com/in/david-chenfitzgerald/" target="_blank"><img src="/content/images/linkedin.png" alt="Linkedin link"></a>
+      <a href="https://github.com/davidfitz314" target="_blank"><img src="/content/images/github-logo.png" alt="Github link"></a>
     </div>
     <div class="noLinkEffects">
       <div>Facebook, Github, and Linkedin Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC BY 3.0</a></div>
     </div>
 </div>
+<!-- copyright and year -->
+<!-- create a second navigation menu -->
+<!-- add in a sitemap after website completion -->
+<!-- create a small recent projects section here, with textual description and links -->
+<!-- keywords for search optimization example: https://www.orbitmedia.com/blog/website-footer-design-best-practices/ -->
+<a href="index.php">sitemap </a><span> <p>&copy; 2018 David ChenFitzgerald</p>
 </footer>
 </body>
 </html>
