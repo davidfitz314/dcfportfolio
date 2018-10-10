8ae95377d379f059b0da3aae326f8cae245cabf9
davidfitz314 <davidfitz314@gmail.com>
2018-10-10 02:06:07 -0600

    adding more images to music encrypted page

--- content/projectFolder/music_encrypt_keyboard.php
+++ content/projectFolder/music_encrypt_keyboard.php
@@ -7,6 +7,8 @@
 <h4>Images</h4>
 <img src="/content/images/Music_Encryption_images/EncryptKeyboard.jpg" alt="Basic Keyboard With onscreen Notes">
 <img src="/content/images/Music_Encryption_images/EncryptKeyboardPressed.jpg" alt="Basic Keyboard With onscreen Note 'E' pressed">
+<img src="/content/images/Music_Encryption_imagesEncrypted_Text.jpg" alt="Encrypted Text">
+<img src="/content/images/Music_Encryption_images/Decrypted_Text.jpg" alt="Decrypted Text">
 <p><i>insert pagination at base of each project page</i></p>
 </div>
 <?php include '../defaultIncludes/footer.php'; ?>
