9560d02e4b34fcf7346f84e014804e15a4b80117
davidfitz314 <davidfitz314@gmail.com>
2018-10-10 01:20:13 -0600

    adding content to the about me page

--- content/aboutMe.php
+++ content/aboutMe.php
@@ -1,14 +1,13 @@
 <?php include 'defaultIncludes/header.php'; ?>
 <div class="centeredWrapper">
   <img src="images/davidNormal1.jpg" alt="Everyday Dave" width="300px" height="300px">
-  <h3>My name is David ChenFitzgerald and I am a Software Developer and IT technician.</h3>
-  <p>I love Computer Programming and Information Technology.<br />
-    I have a Bachelors in CIT emphasizing CS.<br />
-    I am an accomplishment oriented person, it’s similar to goal oriented,
-    but instead, I try to see the big picture alongside of each smaller goal.<br />
-    I am great at working in teams, and I enjoy communicating and getting to know about my project partners.<br />
-    I have been in customer service for quite some time and I have learned to excel at it,
-    by giving customers great support. <br />
-    I am also an easy going person who knows how to get tasks done on time.</p>
+  <p>My name is David ChenFitzgerald. I am a Computer Programmer and It Technician.
+  I live in St. George UT, and I love travelling to new places. I got married in 2017 to a beautiful Chinese graphic web designer and from then on have improved my own apps greatly due to her influence.
+  I currently take on Freelance programming projects the most, because I feel every person has new and interesting app ideas but they just don't know how to publish them, so i do my best to help.
+  </p>
+  <p>Extra Interests</p>
+  <p>I volunteer to help teach kids programming, because I believe the worlds technology will only grow greater with new minds coming to help.
+  I also am a cellist, and truly enjoy incorporating music into my apps.
+  I love encryption, number theory, and statistics. To figure out a new solution to a problem or just to make something better is astounding to me.</p>
 </div>
 <?php include 'defaultIncludes/footer.php'; ?>
