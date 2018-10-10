88a23158b14dbd81186fe4c3345940675ec19fbb
davidfitz314 <davidfitz314@gmail.com>
2018-10-09 22:28:55 -0600

    adding in navigation linnks to the footer

--- content/defaultIncludes/footer.php
+++ content/defaultIncludes/footer.php
@@ -1,29 +1,37 @@
 <footer>
+</div>
+<!-- keywords for search optimization example: https://www.orbitmedia.com/blog/website-footer-design-best-practices/ -->
+<div class="leftFooter">
+  <p>What to say about this website to improve search optimization? I don't know yet but I can add it or a second logo.</p>
+  <div class="connectIcons">
+    <a href="https://www.facebook.com/david.fitzgerald.528" target="_blank"><img src="/content/images/facebook.png" alt="Facebook link"></a>
+    <a href="https://www.linkedin.com/in/david-chenfitzgerald/" target="_blank"><img src="/content/images/linkedin.png" alt="Linkedin link"></a>
+    <a href="https://github.com/davidfitz314" target="_blank"><img src="/content/images/github-logo.png" alt="Github link"></a>
+  </div>
+  <div class="noLinkEffects">
+    <div>Facebook, Github, and Linkedin Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC BY 3.0</a></div>
+  </div>
+</div>
 
-    <div class="connectIcons">
-      <a href="https://www.facebook.com/david.fitzgerald.528" target="_blank"><img src="/content/images/facebook.png" alt="Facebook link"></a>
-      <a href="https://www.linkedin.com/in/david-chenfitzgerald/" target="_blank"><img src="/content/images/linkedin.png" alt="Linkedin link"></a>
-      <a href="https://github.com/davidfitz314" target="_blank"><img src="/content/images/github-logo.png" alt="Github link"></a>
-    </div>
-    <div class="noLinkEffects">
-      <div>Facebook, Github, and Linkedin Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC BY 3.0</a></div>
-    </div>
+<!-- create a small recent projects section here, with textual description and links -->
+<div class="middleFooter">
+  <a href="/content/projectFolder/index.php">The current website that you are on. ;)</a>
+  <a href="/content/projectFolder/OWL-Flashcards.php">Practice learning Chinese/English by using flashcards of English most common words.</a>
+  <a href="/content/projectFolder/birdandb.php">A website designed to help aid learning for biological and psyiological functions in a reporoductive context.</a>
 </div>
 
 <!-- create a second navigation menu -->
-<a href="index.php">Home</a>
-<a href="aboutMe.php">About Me</a>
-<a href="projects.php">Projects</a>
-<a href="education.php">Education</a>
-<!-- add in a sitemap after website completion -->
-<!-- create a small recent projects section here, with textual description and links -->
-<a href="/content/projectFolder/index.php">The current website that you are on. ;)</a>
-<a href="/content/projectFolder/OWL-Flashcards.php">Practice learning Chinese/English by using flashcards of English most common words.</a>
-<a href="/content/projectFolder/birdandb.php">A website designed to help aid learning for biological and psyiological functions in a reporoductive context.</a>
+<div class="rightFooter">
+  <a href="index.php">Home</a>
+  <a href="aboutMe.php">About Me</a>
+  <a href="projects.php">Projects</a>
+  <a href="education.php">Education</a>
+</div>
 
-<!-- keywords for search optimization example: https://www.orbitmedia.com/blog/website-footer-design-best-practices/ -->
+<!-- add in a sitemap after website completion -->
 <!-- copyright and year -->
-<a href="index.php">sitemap </a> <p>&copy; 2018 David ChenFitzgerald</p>
+<a href="index.php">sitemap </a>
+<p>&copy; 2018 David ChenFitzgerald</p>
 </footer>
 </body>
 </html>
