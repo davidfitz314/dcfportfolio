c070c4281b4464853adf382c976fc98df468006d
davidfitz314 <davidfitz314@gmail.com>
2018-10-10 02:09:21 -0600

    minor spelling correction on music encryption page

--- content/projectFolder/music_encrypt_keyboard.php
+++ content/projectFolder/music_encrypt_keyboard.php
@@ -5,10 +5,10 @@
 <h4>OverView</h4>
 <p>Music Encrypting/Decrypting KeyBoard</p>
 <h4>Images</h4>
-<img src="/content/images/Music_Encryption_images/EncryptKeyboard.jpeg" alt="Basic Keyboard With onscreen Notes">
-<img src="/content/images/Music_Encryption_images/EncryptKeyboardPressed.jpeg" alt="Basic Keyboard With onscreen Note 'E' pressed">
-<img src="/content/images/Music_Encryption_imagesEncrypted_Text.jpeg" alt="Encrypted Text">
-<img src="/content/images/Music_Encryption_images/Decrypted_Text.jpeg" alt="Decrypted Text">
+<img src="/content/images/Music_Encryption_Images/EncryptKeyboard.JPG" alt="Basic Keyboard With onscreen Notes">
+<img src="/content/images/Music_Encryption_Images/EncryptKeyboardPressed.JPG" alt="Basic Keyboard With onscreen Note 'E' pressed">
+<img src="/content/images/Music_Encryption_ImagesEncrypted_Text.JPG" alt="Encrypted Text">
+<img src="/content/images/Music_Encryption_Images/Decrypted_Text.JPG" alt="Decrypted Text">
 <p><i>insert pagination at base of each project page</i></p>
 </div>
 <?php include '../defaultIncludes/footer.php'; ?>
