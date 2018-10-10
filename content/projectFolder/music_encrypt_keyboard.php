5748a2b5454fba1f2622a3d5e15b1ae7df1b1793
davidfitz314 <davidfitz314@gmail.com>
2018-10-10 02:18:41 -0600

    adding link to outside site showing source code of project

--- content/projectFolder/music_encrypt_keyboard.php
+++ content/projectFolder/music_encrypt_keyboard.php
@@ -4,6 +4,7 @@
 <hr />
 <h4>OverView</h4>
 <p>Music Encrypting/Decrypting KeyBoard</p>
+<p>For Source Code of this project please <a href="https://github.com/davidfitz314/Music-Encryption" target="_blank">click here</a></p>
 <h4>Images</h4>
 <img src="/content/images/Music_Encryption_Images/EncryptKeyBoard.JPG" alt="Basic Keyboard With onscreen Notes">
 <img src="/content/images/Music_Encryption_Images/EncryptKeyboardKeyPressed.JPG" alt="Basic Keyboard With onscreen Note 'E' pressed">
