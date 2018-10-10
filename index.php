0dee487a0fc78458abef13ef07c0b03437d010ca
davidfitz314 <davidfitz314@gmail.com>
2018-10-10 01:08:18 -0600

    updating about me on the index page

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
         <!-- <h3>My name is David ChenFitzgerald and I am a Software Developer and IT technician.</h3>
         --><p>My name is David ChenFitzgerald. I am a Computer Programmer and It Technician.
         I live in St. George UT, and I love travelling to new places. I got married in 2017 to a beautiful Chinese graphic web designer and from then on have improved my own apps greatly due to her influence.
-        I currently take on Freelance programming projects the most, because I feel every person has new and interesting app ideas but they just don't know how to publish them, so i do my best to help... </p>
+        I currently take on Freelance programming projects the most, because I feel every person has new and interesting app ideas but they just don't know how to publish them, so i do my best to help.
+        I have done some volunteering to help teach kids programming, because I believe the worlds technology will only grow greater with new minds coming to help.</p>
         <p>continue reading...</p>
       </div>
       <div class="AboutMeWrapperGoRight">
