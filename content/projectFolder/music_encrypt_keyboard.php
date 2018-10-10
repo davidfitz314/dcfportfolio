c4e424399cb5c835f9548f755ab9a300f33df8eb
davidfitz314 <davidfitz314@gmail.com>
2018-10-10 02:08:44 -0600

    renaming the image folder for storing music encryption images

--- content/projectFolder/music_encrypt_keyboard.php
+++ content/projectFolder/music_encrypt_keyboard.php
@@ -5,10 +5,10 @@
 <h4>OverView</h4>
 <p>Music Encrypting/Decrypting KeyBoard</p>
 <h4>Images</h4>
-<img src="/content/images/Music_Encryption_images/EncryptKeyboard.jpg" alt="Basic Keyboard With onscreen Notes">
-<img src="/content/images/Music_Encryption_images/EncryptKeyboardPressed.jpg" alt="Basic Keyboard With onscreen Note 'E' pressed">
-<img src="/content/images/Music_Encryption_imagesEncrypted_Text.jpg" alt="Encrypted Text">
-<img src="/content/images/Music_Encryption_images/Decrypted_Text.jpg" alt="Decrypted Text">
+<img src="/content/images/Music_Encryption_images/EncryptKeyboard.jpeg" alt="Basic Keyboard With onscreen Notes">
+<img src="/content/images/Music_Encryption_images/EncryptKeyboardPressed.jpeg" alt="Basic Keyboard With onscreen Note 'E' pressed">
+<img src="/content/images/Music_Encryption_imagesEncrypted_Text.jpeg" alt="Encrypted Text">
+<img src="/content/images/Music_Encryption_images/Decrypted_Text.jpeg" alt="Decrypted Text">
 <p><i>insert pagination at base of each project page</i></p>
 </div>
 <?php include '../defaultIncludes/footer.php'; ?>
