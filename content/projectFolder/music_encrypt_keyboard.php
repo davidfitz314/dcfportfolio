bb397581b50421d9034499655995a4d5ac36943d
davidfitz314 <davidfitz314@gmail.com>
2018-10-11 23:31:56 -0600

    creating a default look for indivual projects pages

--- content/projectFolder/music_encrypt_keyboard.php
+++ content/projectFolder/music_encrypt_keyboard.php
@@ -1,15 +1,17 @@
 <?php include '../defaultIncludes/header.php'; ?>
 <div class="centeredWrapper">
-<a href="../projects">Return to projects directory</a>
-<hr />
-<h4>OverView</h4>
-<p>Music Encrypting/Decrypting KeyBoard</p>
-<p>For Source Code of this project please <a href="https://github.com/davidfitz314/Music-Encryption" target="_blank">click here</a></p>
-<h4>Images</h4>
-<img src="/content/images/Music_Encryption_Images/EncryptKeyBoard.JPG" alt="Basic Keyboard With onscreen Notes">
-<img src="/content/images/Music_Encryption_Images/EncryptKeyboardKeyPressed.JPG" alt="Basic Keyboard With onscreen Note 'E' pressed">
-<img src="/content/images/Music_Encryption_Images/Encrypted_Text.JPG" alt="Encrypted Text">
-<img src="/content/images/Music_Encryption_Images/Decrypted_Text.JPG" alt="Decrypted Text">
-<p><i>insert pagination at base of each project page</i></p>
+  <a href="../projects">Return to projects directory</a>
+  <div class="basicProjecetLayout">
+    <hr />
+    <h4>OverView</h4>
+    <p>Music Encrypting/Decrypting KeyBoard</p>
+    <p>For Source Code of this project please <a href="https://github.com/davidfitz314/Music-Encryption" target="_blank">click here</a></p>
+    <h4>Images</h4>
+    <img src="/content/images/Music_Encryption_Images/EncryptKeyBoard.JPG" alt="Basic Keyboard With onscreen Notes">
+    <img src="/content/images/Music_Encryption_Images/EncryptKeyboardKeyPressed.JPG" alt="Basic Keyboard With onscreen Note 'E' pressed">
+    <img src="/content/images/Music_Encryption_Images/Encrypted_Text.JPG" alt="Encrypted Text">
+    <img src="/content/images/Music_Encryption_Images/Decrypted_Text.JPG" alt="Decrypted Text">
+    <p><i>insert pagination at base of each project page</i></p>
+  </div>
 </div>
 <?php include '../defaultIncludes/footer.php'; ?>
