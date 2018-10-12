c594c0bceb3972d3edfa8ee92fa1b3dcd1d7a2b5
davidfitz314 <davidfitz314@gmail.com>
2018-10-12 02:09:03 -0600

    adding in a script for slideshow images

--- content/projectFolder/music_encrypt_keyboard.php
+++ content/projectFolder/music_encrypt_keyboard.php
@@ -101,63 +101,7 @@ example: presing the keys '1', '2', and then '3' would create the string "DEF". 
       </div>
     </div>
 
-<script>
-// Open the Modal
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-// Handle ESC key (key code 27)
-document.addEventListener('keyup', function(e) {
-    if (e.keyCode == 27) {
-        closeModal();
-    }
-    if (e.keyCode == 39) {
-        plusSlides(1);
-    }
-    if (e.keyCode == 37) {
-        plusSlides(-1);
-    }
-});
-
-
-</script>
+<?php include '../defaultIncludes/JavascriptSlideShowScript.php'; ?> 
 
     <p><i>insert pagination at base of each project page</i></p>
   </div>
