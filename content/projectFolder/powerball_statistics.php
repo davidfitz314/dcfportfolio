59905f8a29939891ff7192302bfc57b9b080eb81
davidfitz314 <davidfitz314@gmail.com>
2018-10-17 17:50:00 -0600

    adding in powerball stats lilnk in project page

--- content/projectFolder/powerball_statistics.php
+++ content/projectFolder/powerball_statistics.php
@@ -8,7 +8,7 @@
     <p>
       Just a calculation program of the frequency rates on the popular gambling game PowerBall
 <br />
-Written in C++ using microsoft visual studio, this work is property of David Fitzgerald, contributers are welcome to use as long as reference for original content is used.
+Written in C++ using microsoft visual studio.
 <br />
 To update powerball inputs, go to the powerball.txt file and add each element in seperating each number with a space and then for different powerball draws seperate them with a new line.
 <br />
