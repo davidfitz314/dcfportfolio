5384a50ab9ab734a7e61d6e0f27aadd9f208250a
davidfitz314 <davidfitz314@gmail.com>
2018-10-18 00:20:38 -0600

    testing second logo in footer

--- content/defaultIncludes/footer.php
+++ content/defaultIncludes/footer.php
@@ -6,7 +6,8 @@
   <!-- keywords for search optimization example: https://www.orbitmedia.com/blog/website-footer-design-best-practices/ -->
   <div class="leftFooter">
     <h4>connect</h4>
-    <p>What to say about this website to improve search optimization? I don't know yet but I can add it or a second logo.</p>
+    <img src="/content/images/david_logo_D.png" alt="David Logo 2">
+
     <div class="connectIcons">
       <a href="https://www.facebook.com/david.fitzgerald.528" target="_blank"><img src="/content/images/facebook_edit.png" alt="Facebook link"></a>
       <a href="https://www.linkedin.com/in/david-chenfitzgerald/" target="_blank"><img src="/content/images/linkedin_edit.png" alt="Linkedin link"></a>
