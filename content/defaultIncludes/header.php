0a331cacf67aa5fa740b5def54d47eb2653985aa
davidfitz314 <davidfitz314@gmail.com>
2018-10-18 20:18:33 -0600

    testing different menus for different screen sizes auto

--- content/defaultIncludes/header.php
+++ content/defaultIncludes/header.php
@@ -20,17 +20,16 @@
         <button><a href="/content/experience">Experience</a></button>
         <button><a href="/content/projects">Projects</a></button>
 
-
-        <div class="goRight">
-          <!--creating a dropdown menu-->
-          <div class="dropdown">
-            <button class="dropbtn"><img src="/content/images/menuButton.png" alt="menu-button" width="43px" height="auto"></button>
-            <div class="dropdown-content">
-              <a href="/content/aboutMe">About</a>
-              <a href="/content/education">Education</a>
-              <a href="/content/experience">Experience</a>
-              <a href="/content/projects">Projects</a>
-            </div>
+      </div>
+      <div class="goRight">
+        <!--creating a dropdown menu-->
+        <div class="dropdown">
+          <button class="dropbtn"><img src="/content/images/menuButton.png" alt="menu-button" width="43px" height="auto"></button>
+          <div class="dropdown-content">
+            <a href="/content/aboutMe">About</a>
+            <a href="/content/education">Education</a>
+            <a href="/content/experience">Experience</a>
+            <a href="/content/projects">Projects</a>
           </div>
         </div>
       </div>
