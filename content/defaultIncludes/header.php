06ccc1acfa589f944458ce9d2c669527f84395b9
davidfitz314 <davidfitz314@gmail.com>
2018-10-23 02:05:50 -0600

    adding meta tags and data to the generic multipage header

--- content/defaultIncludes/header.php
+++ content/defaultIncludes/header.php
@@ -3,7 +3,11 @@
 <head>
   <link rel="stylesheet" type="text/css" href="/views/baseStyle.css">
   <link rel="shortcut icon" type="image/x-icon" href="/content/images/favicon.ico">
+  <meta charset="utf-8">
+  <meta name="description" content="David's Portfolio">
+  <meta name="keywords" content="portfolio, resume, education, projects, experience, david, information technology, computer science">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="author" content="David ChenFitzgerald">
   <title>DCFPortfolio</title>
 </head>
 <body>
