bfe938721eaf3f24e36fda97d33853ca99ffec41
davidfitz314 <davidfitz314@gmail.com>
2018-10-23 01:35:10 -0600

    adding in a sitemap

--- content/defaultIncludes/footer.php
+++ content/defaultIncludes/footer.php
@@ -41,7 +41,7 @@
 <br />
 <!-- add in a sitemap after website completion -->
 <!-- copyright and year -->
-<a href="index.php">sitemap </a>
+<a href="/content/sitemap.xml">sitemap </a>
 <p>&copy; 2018 David ChenFitzgerald</p>
 
 </footer>
