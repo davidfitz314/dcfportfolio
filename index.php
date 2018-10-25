dff82740ce6844bcc8675bc746fbd12ad5e13ea9
davidfitz314 <davidfitz314@gmail.com>
2018-10-25 04:06:53 -0600

    changing links to smaller areas instead of full paragraphs on index page

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
   <!-- links to about section -->
   <div class="aboutBanner">
     <div class="bannerLinks">
-    <a href="/content/aboutMe">
       <div class="AboutMeWrapperGoLeft">
         <h2>Welcome to my Portfolio,</h2>
         <!-- <h3>My name is David ChenFitzgerald and I am a Software Developer and IT technician.</h3>
         --><p>My name is David ChenFitzgerald. I am a Computer Programmer and It Technician.
         I live in St. George UT, and I love travelling to new places. I got married in 2017 to a beautiful Chinese graphic web designer and from then on have improved my own apps greatly due to her influence.
         I currently take on Freelance programming projects the most, because I feel every person has new and interesting app ideas but they just don't know how to publish them, so i do my best to help.</p>
-        <p>continue reading...</p>
+      <strong>
+        <a href="/content/aboutMe">View More</a>
+      </strong>
       </div>
       <div class="AboutMeWrapperGoRight">
         <img src="/content/images/davidNormal1.jpg" alt="Everyday Dave" width="250px" height="250px">
       </div>
-    </a>
+
     </div>
   </div>
 
@@ -29,51 +30,45 @@
 
   <div class="educationBanner">
     <div class="educationLeft">
-      <a href="/content/education">
-        <img src="/content/images/education-icon.png" alt="Education">
-        <h4>Education</h4>
-        <p>2017
-          <br />
-          Dixie State University
-          <br />
-          225 S. 700 E. St. George, UT. 84790
-          <br />
-          BS Computer Information Technology -Emphasis Computer Science
-        </p>
-        <strong>
-          <p>View More</p>
-        </strong>
-      </a>
+      <img src="/content/images/education-icon.png" alt="Education">
+      <h4>Education</h4>
+      <p>2017
+        <br />
+        Dixie State University
+        <br />
+        225 S. 700 E. St. George, UT. 84790
+        <br />
+        BS Computer Information Technology -Emphasis Computer Science
+      </p>
+    <strong>
+      <a href="/content/education">View More</a>
+    </strong>
     </div>
     <div class="educationMiddle">
-      <a href="/content/experience">
-        <img src="/content/images/experience-icon.png" alt="Experience">
-        <h4>Experience</h4>
-          <br />
-          St. George, UT.
-          <br />
-          2 Years networking, router and wiring installation, testing/debugging networks through the residential side of demarcation.
-        </p>
-        <strong>
-          <p>View More</p>
-        </strong>
-      </a>
+      <img src="/content/images/experience-icon.png" alt="Experience">
+      <h4>Experience</h4>
+        <br />
+        St. George, UT.
+        <br />
+        2 Years networking, router and wiring installation, testing/debugging networks through the residential side of demarcation.
+      </p>
+      <strong>
+        <a href="/content/experience">View More</a>
+      </strong>
     </div>
     <div class="educationRight">
-      <a href="/content/experience#volunteering">
-        <img src="/content/images/volunteer-icon.png" alt="Volunteer">
-        <h4>Volunteering</h4>
-          <p>2016 <br />
-          <strong>DSU 4H Club</strong>
-          <br />
-          St. George, UT.
-          <br />
-          Taught introductory programming courses to children aged 10 - 15 years old.
-        </p>
-        <strong>
-          <p>View More</p>
-        </strong>
-      </a>
+      <img src="/content/images/volunteer-icon.png" alt="Volunteer">
+      <h4>Volunteering</h4>
+        <p>2016 <br />
+        <strong>DSU 4H Club</strong>
+        <br />
+        St. George, UT.
+        <br />
+        Taught introductory programming courses to children aged 10 - 15 years old.
+      </p>
+      <strong>
+        <a href="/content/experience#volunteering">View More</a>
+      </strong>
     </div>
   </div>
 
