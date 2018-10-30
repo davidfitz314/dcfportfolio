fe3b3e9ed88ba22f633ac7532054e7e5c06d1577
davidfitz314 <davidfitz314@gmail.com>
2018-10-29 23:38:34 -0600

    modifying the design for the about page

--- content/aboutMe.php
+++ content/aboutMe.php
@@ -1,17 +1,24 @@
 <?php include 'defaultIncludes/header.php'; ?>
-<div class="centeredWrapper">
-  <img src="images/davidNormal1.jpg" alt="Everyday Dave" width="300px" height="300px">
-  <p>My name is David ChenFitzgerald. I am a Computer Programmer and It Technician.
-  I live in St. George UT, and I love travelling to new places. I got married in 2017 to a beautiful Chinese graphic web designer and from then on have improved my own apps greatly due to her influence.
-  I currently take on freelance programming projects the most, because I feel every person has new and interesting app ideas, but they just don't know how to publish them, so i do my best to help.
-  </p>
-  <p>Extra Interests</p>
-  <p>I volunteer to help teach kids programming, because I believe the worlds technology will only grow greater with new minds coming to help.
-  I am also a cellist, and truly enjoy incorporating music into my apps.
-  I love encryption, number theory, and statistics, to figure out a new solution to a problem or just to make something better is astounding to me.</p>
-  <p>To see details regarding my experience and education, please visit my <a href="/content/education">Education Page</a> and <a href="/content/experience">Experience Page</a>.</p>
-  <p>or</p>
-  <p>To see details of projects i've worked on, please visit my <a href="/content/projects">Projects Page</a>.</p>
-  <!-- <p>and</p> <p>don't forget to contact me for new projects.</p> -->
+<div class="aboutBackground">
+  <div class="aboutMain">
+    <div class="aboutLeft">
+      <img src="images/davidNormal1.jpg" alt="Everyday Dave">
+    </div>
+    <div class="aboutRight">
+      <p>My name is David ChenFitzgerald. I am a Computer Programmer and It Technician.
+      I live in St. George UT, and I love travelling to new places. I got married in 2017 to a beautiful Chinese graphic web designer and from then on have improved my own apps greatly due to her influence.
+      I currently take on freelance programming projects the most, because I feel every person has new and interesting app ideas, but they just don't know how to publish them, so i do my best to help.
+      </p>
+    </div>
+  </div>
 </div>
+  <div class="aboutMain">
+    <p>Extra Interests</p>
+    <p>I volunteer to help teach kids programming, because I believe the worlds technology will only grow greater with new minds coming to help.
+    I am also a cellist, and truly enjoy incorporating music into my apps.
+    I love encryption, number theory, and statistics, to figure out a new solution to a problem or just to make something better is astounding to me.</p>
+    <p>To see details regarding my experience and education, please visit my <a href="/content/education">Education Page</a> and <a href="/content/experience">Experience Page</a>.</p>
+    <p>or</p>
+    <p>To see details of projects i've worked on, please visit my <a href="/content/projects">Projects Page</a>.</p>
+  </div>
 <?php include 'defaultIncludes/footer.php'; ?>
