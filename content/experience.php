4ea88b11398c33fff0e92e511842fb763aa9565c
davidfitz314 <davidfitz314@gmail.com>
2018-10-30 00:52:02 -0600

    updating experience design

--- content/experience.php
+++ content/experience.php
@@ -6,30 +6,39 @@
     </h1>
   </div>
 </div>
-<img src="/content/images/experience-icon.png" alt="Experience">
-<h3>Experience</h3>
-<p><strong>DSU Disability Resource Center</strong>
-  <br />
-  St. George, UT
-  <br />
-  Title: <i>Visual Software Translator</i>
-  <br />
-  Translated visual software, written words, images, and code into a format that a vision impaired student could access for his Artificial Intelligence class.
-</p>
-<p><strong>Multiple Locations</strong>
-  <br />
-  10 years of software experience in regards to cash register systems, office docs, and online employee portals.
-  <br />
-  10+ years customer service and sales experience.
-</p>
-<p><strong>FreeLance Experience</strong>
-  <br />
-  2 years of networking, including setting up local routers, installing new wires, installing updates, and testing / debugging through the residentail demarcation.
-  <br />
-  1 year of installing operating systems for linux, windows, and mac os.
-  <br />
-  3 years of system administration (non server based) ; including installing software, cleaning up viruses and caches, setting up new devices, and upkeeping system performance.
-</p>
+
+<div class="expBackground">
+  <div class="expMain">
+    <div class="expLeft">
+      <img src="/content/images/experience-icon.png" alt="Experience">
+    </div>
+    <div class="expRight">
+      <h2>WORK EXPERIENCE</h2>
+      <p><strong>DSU Disability Resource Center</strong>
+        <br />
+        St. George, UT
+        <br />
+        Title: <i>Visual Software Translator</i>
+        <br />
+        Translated visual software, written words, images, and code into a format that a vision impaired student could access for his Artificial Intelligence class.
+      </p>
+      <p><strong>Multiple Locations</strong>
+        <br />
+        10 years of software experience in regards to cash register systems, office docs, and online employee portals.
+        <br />
+        10+ years customer service and sales experience.
+      </p>
+      <p><strong>FreeLance Experience</strong>
+        <br />
+        2 years of networking, including setting up local routers, installing new wires, installing updates, and testing / debugging through the residentail demarcation.
+        <br />
+        1 year of installing operating systems for linux, windows, and mac os.
+        <br />
+        3 years of system administration (non server based) ; including installing software, cleaning up viruses and caches, setting up new devices, and upkeeping system performance.
+      </p>
+    </div>
+  </div>
+</div>
 
 <br />
 <img src="/content/images/volunteer-icon.png" alt="Volunteer">
