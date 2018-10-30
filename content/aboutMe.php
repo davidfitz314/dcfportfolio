296d99c73383ce17e4c270c12919fc55488a5147
davidfitz314 <davidfitz314@gmail.com>
2018-10-29 23:49:21 -0600

    modifying the design for the about page

--- content/aboutMe.php
+++ content/aboutMe.php
@@ -5,6 +5,7 @@
       <img src="images/davidNormal1.jpg" alt="Everyday Dave">
     </div>
     <div class="aboutRight">
+      <h2>BIO</h2>
       <p>My name is David ChenFitzgerald. I am a Computer Programmer and It Technician.
       I live in St. George UT, and I love travelling to new places. I got married in 2017 to a beautiful Chinese graphic web designer and from then on have improved my own apps greatly due to her influence.
       I currently take on freelance programming projects the most, because I feel every person has new and interesting app ideas, but they just don't know how to publish them, so i do my best to help.
