79e5fc598d465c38c092ada95110cb5fee695b66
davidfitz314 <davidfitz314@gmail.com>
2018-10-29 23:48:24 -0600

    modifying the design for the about page

--- content/aboutMe.php
+++ content/aboutMe.php
@@ -12,7 +12,7 @@
     </div>
   </div>
 </div>
-  <div class="aboutMain">
+  <div class="aboutMainpt2">
     <p>Extra Interests</p>
     <p>I volunteer to help teach kids programming, because I believe the worlds technology will only grow greater with new minds coming to help.
     I am also a cellist, and truly enjoy incorporating music into my apps.
