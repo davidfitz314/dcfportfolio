6799b421b550c1177532211d5e90c31ec21ad353
davidfitz314 <davidfitz314@gmail.com>
2018-10-31 03:18:08 -0600

    updating design on project folder page

--- content/projects.php
+++ content/projects.php
@@ -58,18 +58,7 @@
         <p><strong>3D Rat Maze</strong>. User controlled rat and auto generated maze layout.</p>
       </a>
     </div>
-
   </div>
 </div>
-
-<button><a href="projectFolder/OWL-FlashCards">OWL-FlashCards</a></button>
-<button><a href="projectFolder/birdandb">BirdandB.com</a></button>
-<button><a href="projectFolder/music_encrypt_keyboard">Music Encryption Keyboard</a></button>
 <br />
-<button><a href="projectFolder/powerball_statistics">Powerball Statistics</a></button>
-<button><a href="projectFolder/JSMaxPriorityQueue">JavaScript Max Priority Queue</a></button>
-<button><a href="projectFolder/Auto_Chess_Story">Automated Chess Story</a></button>
-<button><a href="projectFolder/3d_rat_maze">3D Rat Maze</a></button>
-
-
 <?php include 'defaultIncludes/footer.php'; ?>
