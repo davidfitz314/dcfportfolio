c31d15481d78a0def1c7e5d3e0bf7b66e423dcd2
davidfitz314 <davidfitz314@gmail.com>
2018-10-31 01:19:17 -0600

    adding content and designs to the education page

--- content/education.php
+++ content/education.php
@@ -2,10 +2,47 @@
 <div class="eduMain">
   <div class="eduHead">
     <h1>
-      EDUCATION
+      CERTIFICATES & EDUCATION
     </h1>
   </div>
 </div>
+<div class="eduBackground">
+  <div class="eduMain">
+    <div class="eduLeft">
+    </div>
+    <div class="eduright">
+    </div>
+  </div>
+</div>
+
+
+<div class="eduBackground">
+  <div class="eduMain">
+    <div class="eduLeft">
+      <img src="/content/images/appcertification.png" alt="Android App Certfication">
+    </div>
+    <div class="eduright">
+      <h2>Certifications</h2>
+      <p>Google Andoid App Developer Assosciates Certification
+        <br />
+      ...in progress...</p>
+    </div>
+  </div>
+</div>
+
+<div class="edumain2">
+  <img src="/content/images/1030_1.jpg" alt="Dixie State University">
+  <h2>BACHELORS DEGREE</h2>
+  <p><strong>Dixie State University</strong>
+    <br />
+    225 S. 700 E. St. George, UT. 84790
+    <br />
+    Bachelors of Science in Computer Information Technology emphasis Computer Science
+    <br />
+    BS:CIT-CS
+  </p>
+</div>
+
 <img src="/content/images/education-icon.png" alt="Education">
 <h3>Education</h3>
 <img src="images/dsu-logo.jpg" alt="Dixie State University">
