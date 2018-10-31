d124a257c49ee3e85e0575087930525782795a4f
davidfitz314 <davidfitz314@gmail.com>
2018-10-31 01:36:33 -0600

    adding content and designs to the education page

--- content/education.php
+++ content/education.php
@@ -43,17 +43,4 @@
   </p>
 </div>
 
-<img src="/content/images/education-icon.png" alt="Education">
-<h3>Education</h3>
-<img src="images/dsu-logo.jpg" alt="Dixie State University">
-<h4>Dixie State University</h4>
-<p>225 S. 700 E. St. George, UT. 84790
-  <br />
-  <strong><i>Bachelors Computer Information Technology Emphasis Computer Science</i></strong>
-</p>
-<p><strong>Degree Course Work Section?</strong></p>
-<h3>Certificates</h3>
-<p><i>insert certificate image here?</i></p>
-<p>Google App Developer Associate Certificate...<i> in progress</i>.</p>
-
 <?php include 'defaultIncludes/footer.php'; ?>
