6cd71549052f0bbe37b8d4b1855c6efb9eacaf62
davidfitz314 <davidfitz314@gmail.com>
2018-10-31 03:11:27 -0600

    updating design on project folder page

--- content/projects.php
+++ content/projects.php
@@ -8,56 +8,57 @@
 </div>
 <div class="projMain">
   <div class="projRows">
-    <a href="projectFolder/birdandb">
-      <div class="projR1">
+    <div class="projR1">
+      <a href="projectFolder/birdandb">
         <img src="/content/images/Birdandb_Images/birdandbCover.jpg" alt="BirdAndB.com">
         <p><strong>BirdanB.com</strong>. A educational website for sex, culture, and behavior.</p>
-      </div>
-    </a>
+      </a>
+    </div>
     <div class="projSpacer">
     </div>
-    <a href="projectFolder/music_encrypt_keyboard">
-      <div class="projR1">
+    <div class="projR1">
+      <a href="projectFolder/music_encrypt_keyboard">
         <img src="/content/images/Music_Encryption_Images/EncryptKeyboardCover.jpg" alt="Music Encrypt Key board">
         <p><strong>Music Encryption Keyboard</strong>. Encrypt text using musical frequencies and keyboard notes.</p>
-      </div>
-    </a>
+      </a>
+    </div>
   </div>
 
   <div class="projRows">
-    <a href="projectFolder/music_encrypt_keyboard">
-      <div class="projR1">
+    <div class="projR1">
+      <a href="projectFolder/music_encrypt_keyboard">
         <img src="/content/images/PowerballStatisticsImg/PBStatsCover.jpg" alt="Powerball Statitics">
         <p><strong>Powerball Statistics</strong>. Powerball statistics and number popularity generator.</p>
-      </div>
-    </a>
+      </a>
+    </div>
     <div class="projSpacer">
     </div>
-    <a href="projectFolder/powerball_statistics">
-      <div class="projR1">
+    <div class="projR1">
+      <a href="projectFolder/powerball_statistics">
         <img src="/content/images/JSMQueueImg/JSMCover.jpg" alt="JavaScript Max Priority Queue">
         <p><strong>JavaScript Max Priority Queue</strong>. Max out Priority queue using JavaScript.</p>
-      </div>
-    </a>
+      </a>
+    </div>
   </div>
 
   <div class="projRows">
 
     <div class="projR1">
-      <!--<a href="projectFolder/Auto_Chess_Story">-->
+      <a href="projectFolder/Auto_Chess_Story">
         <img src="/content/images/AutoChessImg/AutoChessStoryCover.jpg" alt="Automated Chess Story">
         <p><strong>Auto Chess Story</strong>. Glut 3d drawings for an automated chess storyline.</p>
-      <!--</a>-->
+      </a>
     </div>
 
     <div class="projSpacer">
     </div>
-    <a href="projectFolder/3d_rat_maze">
-      <div class="projR1">
+    <div class="projR1">
+      <a href="projectFolder/3d_rat_maze">
         <img src="/content/images/3dRatMazeImg/3dRatMazeCover.jpg" alt="3D Rat Maze">
         <p><strong>3D Rat Maze</strong>. User controlled rat and auto generated maze layout.</p>
-      </div>
-    </a>
+      </a>
+    </div>
+
   </div>
 </div>
 
