250b71d70971972235e0d7b4f1ebc041a5577735
davidfitz314 <davidfitz314@gmail.com>
2018-11-03 14:28:35 -0600

    updating education page layout

--- content/education.php
+++ content/education.php
@@ -31,16 +31,20 @@
 </div>
 
 <div class="eduMain2">
-  <img src="/content/images/1101_1.jpg" alt="Dixie State University">
-  <h2>BACHELORS DEGREE</h2>
-  <p><strong>Dixie State University</strong>
-    <br />
-    225 S. 700 E. St. George, UT. 84790
-    <br />
-    <strong><i>Bachelors of Science in Computer Information Technology emphasis Computer Science</i></strong>
-    <br />
-    Abrreviated <strong>BS.CIT-CS</strong>
-  </p>
+  <div class="eduRight2">
+    <img src="/content/images/1030_1.jpg" alt="Dixie State University">
+  </div>
+  <div class="eduLeft2">
+    <h2>BACHELORS DEGREE</h2>
+    <p><strong>Dixie State University</strong>
+      <br />
+      225 S. 700 E. St. George, UT. 84790
+      <br />
+      <strong><i>Bachelors of Science in Computer Information Technology emphasis Computer Science</i></strong>
+      <br />
+      Abrreviated <strong>BS.CIT-CS</strong>
+    </p>
+  </div>
 </div>
 
 <?php include 'defaultIncludes/footer.php'; ?>
