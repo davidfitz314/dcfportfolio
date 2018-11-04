f24c8f8d9c0ae7894c192fe05d8b3298fb84c543
davidfitz314 <davidfitz314@gmail.com>
2018-11-04 15:27:28 -0700

    new slideshow for projects

--- content/defaultIncludes/JavascriptSlideShowScript.php
+++ content/defaultIncludes/JavascriptSlideShowScript.php
@@ -8,17 +8,12 @@ function currentDiv(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("slideshowImgs");
-  var dots = document.getElementsByClassName("demo");
+  var x = document.getElementsByClassName("eachSlide");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-red", "");
-  }
   x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " w3-red";
 }
 </script>
