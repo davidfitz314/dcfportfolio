1b58996fac822338ec00320384400b1c22ba9b14
davidfitz314 <davidfitz314@gmail.com>
2018-11-05 15:47:36 -0700

    testing new project page layout

--- content/projectFolder/3d_rat_maze.php
+++ content/projectFolder/3d_rat_maze.php
@@ -5,33 +5,37 @@
     <hr />
     <h3>3D Rat Maze</h3>
     <h4>OverView</h4>
-    <p>
-      C++, 3D Glut, and Wondows Visual Studio project.
-      <br />
-      Generates a radmonizecd maze within set area, with overlapping pictures for walls, flooring, and ceiling.
-      <br />
-      A user controlled mouse is placed on the starting point (blue floor tile), and attempts to reach the end (green floor tile).
-      <br />
-      Mouse moves with user mouse clicks left and right for turning and center button for forward.
-      <br />
-      In event there is no center mouse button keyboard buttons 'z' and 'x' act as a replacement, for starting movement and stopping.
-    </p>
 
-    <h4>External Links</h4>
-    <p>For Source Code of this project please <a href="https://github.com/davidfitz314/RatMaze-3d-Assignment" target="_blank">click here</a></p>
-    <h4>Images</h4>
+    <div class="groupLayout">
+      <div class="singleProjRight">
+        <p>
+          C++, 3D Glut, and Wondows Visual Studio project.
+          <br />
+          Generates a radmonizecd maze within set area, with overlapping pictures for walls, flooring, and ceiling.
+          <br />
+          A user controlled mouse is placed on the starting point (blue floor tile), and attempts to reach the end (green floor tile).
+          <br />
+          Mouse moves with user mouse clicks left and right for turning and center button for forward.
+          <br />
+          In event there is no center mouse button keyboard buttons 'z' and 'x' act as a replacement, for starting movement and stopping.
+        </p>
+        <button> <a href="https://github.com/davidfitz314/RatMaze-3d-Assignment" target="_blank">Source Code</a></button>
+      </div>
 
-    <!-- The slide show  -->
-    <div class="slideshowImgs">
-      <img src="/content/images/3dRatMazeImg/3dRatMazeViewTopPerspective.JPG" class="eachSlide">
-      <img src="/content/images/3dRatMazeImg/3dRatMazeView1.JPG" class="eachSlide">
-      <img src="/content/images/3dRatMazeImg/3dRatMazeView2.JPG" class="eachSlide">
-      <img src="/content/images/3dRatMazeImg/3dRatMazeViewGoalView.JPG" class="eachSlide">
-      <img src="/content/images/3dRatMazeImg/3dRatMazeFilesOverView.JPG" class="eachSlide">
-      <img src="/content/images/3dRatMazeImg/3dRatMazeHeaderCode.JPG" class="eachSlide">
-      <img src="/content/images/3dRatMazeImg/3dRatMazeRatHeaderCode.JPG" class="eachSlide">
+      <h4>Images</h4>
+      <div class="singleProjLeft">
+      <!-- The slide show  -->
+        <div class="slideshowImgs">
+          <img src="/content/images/3dRatMazeImg/3dRatMazeViewTopPerspective.JPG" class="eachSlide">
+          <img src="/content/images/3dRatMazeImg/3dRatMazeView1.JPG" class="eachSlide">
+          <img src="/content/images/3dRatMazeImg/3dRatMazeView2.JPG" class="eachSlide">
+          <img src="/content/images/3dRatMazeImg/3dRatMazeViewGoalView.JPG" class="eachSlide">
+          <img src="/content/images/3dRatMazeImg/3dRatMazeFilesOverView.JPG" class="eachSlide">
+          <img src="/content/images/3dRatMazeImg/3dRatMazeHeaderCode.JPG" class="eachSlide">
+          <img src="/content/images/3dRatMazeImg/3dRatMazeRatHeaderCode.JPG" class="eachSlide">
+        </div>
+      </div>
     </div>
-
     <div class="imgButtons">
       <img onclick="currentSlide(1)" src="/content/images/3dRatMazeImg/3dRatMazeViewTopPerspective.JPG">
       <img onclick="currentSlide(2)" src="/content/images/3dRatMazeImg/3dRatMazeView1.JPG">
