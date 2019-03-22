ba5173c477d3b9d8376b22ccbeddbae0bd352012
davidfitz314 <davidfitz314@gmail.com>
2019-03-21 19:57:51 -0600

    adding in certfication photo

--- content/education.php
+++ content/education.php
@@ -18,12 +18,12 @@
 
 <div class="eduBackground">
   <div class="eduMain">
-    <h2>Certifications</h2>
     <div class="eduLeft">
-      <img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/12721471?key=a77ce754edcbfbb0647c2dda45f25469aa05ef14ee219d0f7c05ad8b99d21727" alt="Android App Certfication Badge">
+      <h2>Certifications</h2>
+      <img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/12721471?key=a77ce754edcbfbb0647c2dda45f25469aa05ef14ee219d0f7c05ad8b99d21727" alt="Google Certification" />
     </div>
     <div class="eduRight">
-      <img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/12721471?key=a77ce754edcbfbb0647c2dda45f25469aa05ef14ee219d0f7c05ad8b99d21727" alt="Google Certification" />
+      <img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/12721471?key=a77ce754edcbfbb0647c2dda45f25469aa05ef14ee219d0f7c05ad8b99d21727" alt="Android App Certfication Badge" />
     </div>
   </div>
 </div>
