73def27c30d34e37a0359577130e5fb1e51671b6
davidfitz314 <davidfitz314@gmail.com>
2019-03-26 23:53:03 -0600

    playing with links

--- index.php
+++ index.php
@@ -135,7 +135,7 @@
     </div>
   </div>
 
-  <p>Temp Link to OWLFlahscards App</p>
+  <a href="/content/projectFolder/OWL-FlashCards">Temp Link to OWLFlahscards App</a>
   <a href='https://play.google.com/store/apps/details?id=com.dcfportfolio.owlflashcardsen_cnlevel1&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_gb/badges/images/generic/en_badge_web_generic.png'/></a>
 
 
