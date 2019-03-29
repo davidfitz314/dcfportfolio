31945bf2f60f2d8e0c50c67783d07ecf0a5f2813
davidfitz314 <davidfitz314@gmail.com>
2019-03-29 00:09:55 -0600

    updating owl links

--- content/projectFolder/OWL-FlashCards.php
+++ content/projectFolder/OWL-FlashCards.php
@@ -29,12 +29,13 @@
 
         <div class="owl_google_decorations">
           <a href='https://play.google.com/store/apps/details?id=com.dcfportfolio.owlflashcardsen_cnlevel1&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_gb/badges/images/generic/en_badge_web_generic.png'/></a>
-          <span class="bluePLink"><a href="/content/images/Downloadables/app.aab" download>Download Directly</a></span>
         </div>
       <span class="bluePLink">
         <a href="https://github.com/davidfitz314/OWLFlashCardsencnlevel1" target="_blank">View Source Code Repository</a>
       </span>
-
+      <span class="bluePLink">
+        <a href="/content/images/Downloadables/app.aab" download>Download App Directly</a>
+      </span>
     </div>
   </div>
 
