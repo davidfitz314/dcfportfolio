00c467ca33ef0a9f180e8e5710ebc970e891478b
davidfitz314 <davidfitz314@gmail.com>
2019-05-16 01:11:30 -0600

    updating text

--- content/aboutMe.php
+++ content/aboutMe.php
@@ -13,8 +13,9 @@
       <h2>BIO</h2>
       <p>My name is David ChenFitzgerald. I am a Freelance Software Developer and IT technician.
         I live in St. George UT, and I love travelling to new places, playing cello, and programming.
-        I enjoy building Android apps the most, the feeling you get when your work is published in the App store and people are actively downloading it, is amazing!
-      </p><p>My goal is to become an expert Android Developer with a large knowledge base of programming, languages, and techniques.
+        I enjoy building Android apps the most, but the feeling you get when you solve any kind of programming problem is amazing.
+        It pushes me forward to learn more and solve even harder problems, simply put it's the most fun/difficult puzzle I could ever play with!
+      </p><p>My goal is to become an expert software developer with a masters degree in computer science.
       </p>
     </div>
   </div>
