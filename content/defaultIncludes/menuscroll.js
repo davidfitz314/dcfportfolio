3df223a0c5cf5003dc32a6389d2b9f282b271068
davidfitz314 <davidfitz314@gmail.com>
2019-10-12 20:53:50 -0600

    added fixed nav function to help with scrolling nav menu

--- content/defaultIncludes/menuscroll.js
+++ content/defaultIncludes/menuscroll.js
@@ -1,3 +1,14 @@
 const nav = document.querySelector('.navMenu');
 let topOfNav = nav.offsetTop;
-console.log(topOfNav);
+
+function fixNav() {
+    if (window.scrollY >= topOfNav) {
+      document.body.style.paddingTop = nav.offsetHeight + 'px';
+      document.body.classList.add('fixed-nav');
+    } else {
+      document.body.classList.remove('fixed-nav');
+      document.body.style.paddingTop = 0;
+    }
+  }
+
+  window.addEventListener('scroll', fixNav);
